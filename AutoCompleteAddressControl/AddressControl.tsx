5c367dc44c31ff591819d79582d0ad77d76eca28
mariovrocha <mario@novalogica.pt>
2024-08-22 01:25:29 +0100

    minor change handling response from mapbox

--- AutoCompleteAddressControl/AddressControl.tsx
+++ AutoCompleteAddressControl/AddressControl.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { SearchBox } from '@mapbox/search-js-react';
-import { IInputs } from './generated/ManifestTypes';
 import { IAddress } from './interfaces/IAddress';
 
 export interface IAddressInputControlProps {
@@ -10,14 +9,14 @@ export interface IAddressInputControlProps {
 
 const AddressControl = ({ mapboxKey, onAddressConfirmed } : IAddressInputControlProps) => {
   const handleChange = (d: any) => {
-    const address = d.features[0]
+    const addressProperties = d.features[0]?.properties;
 
     const result: IAddress = {
-      street: address.properties.context.address?.name ?? address.properties.name ?? "",
-      state: address.properties.context.place?.name ?? address.properties.context.locality?.name ?? "",
-      city: address.properties.context.region?.name ?? "",
-      zipcode: address.properties.context.postcode?.name ?? "",
-      country: address.properties.context.country?.name ?? ""
+      street: addressProperties.context.address?.name ?? addressProperties.name ?? "",
+      state: addressProperties.context.place?.name ?? addressProperties.context.locality?.name ?? "",
+      city: addressProperties.context.region?.name ?? "",
+      zipcode: addressProperties.context.postcode?.name ?? "",
+      country: addressProperties.context.country?.name ?? ""
     };
 
     onAddressConfirmed(result);
